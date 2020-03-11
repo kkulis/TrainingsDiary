9f712bea53bca4df2425b727f0d6a61b924658c2
krzysiekkulig <Krzysiek.kulig@gmail.com>
2020-03-11 17:49:53 +0100

    React Parent Component Done

--- wwwroot/js/reactAddExercise.jsx
+++ wwwroot/js/reactAddExercise.jsx
@@ -1,28 +1,98 @@
 
-
 class AddExercise extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      series: 0
+      numOfSeries: 0
+    }
+  }
+
+  onPlusClick = () => {
+    console.log("+ click!");
+    this.setState(({ numOfSeries }) => ({
+      numOfSeries: numOfSeries + 1
+    }));
+  }
+
+  onMinusClick = () => {
+    console.log("- click!");
+    if (this.state.numOfSeries > 0) {
+      this.setState(({ numOfSeries }) => ({
+        numOfSeries: numOfSeries + -1
+      }));
+    }
+    else {
+      this.setState({
+        numOfSeries: 0
+      })
     }
   }
   render() {
+
     return (
       <div>
-        <label for="numberofSeries">Number of Series</label>
-        <div class="input-group">
-
-          <input type="number" class="form-control" id="numberofSeries" placeholder="amount" />
-          <div class="input-group-append">
-            <button class="btn btn-primary" id="addSeries">+</button>
-            <button class="btn btn-dark" id="removeSeries">-</button>
+        <label htmlFor="numberofSeries">Number of Series</label>
+        <div className="input-group">
+          <InputNumber numOfSeries={this.state.numOfSeries} />
+          <div className="input-group-append">
+            <AddSeriesButton onPlusClick={this.onPlusClick} />
+            <RemoveSeriesButton onMinusClick={this.onMinusClick} />
           </div>
         </div>
 
       </div>
+    );
+  }
+}
+
+class NewSeries extends React.Component{
+
+}
+
+class AddSeriesButton extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.onPlusClick = props.onPlusClick;
+
+  }
+
+  render() {
+    return (
+      <button className="btn btn-primary" onClick={this.onPlusClick}>+</button>
     )
   }
+}
+
+class RemoveSeriesButton extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.onMinusClick = props.onMinusClick;
+
+  }
+
+  render() {
+    return (
+      <button className="btn btn-dark" onClick={this.onMinusClick}>-</button>
+    )
+  }
+}
+
+
+class InputNumber extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      numOfSeries: props.numOfSeries
+    }
+  }
+  render() {
+    return (
+      <input type="number" className="form-control" id="numberOfSeries" placeholder="amount" value={this.props.numOfSeries}></input>
+    );
+  }
+
 }
 
 ReactDOM.render(<AddExercise />, document.getElementById('AddExerciseContainer'));
